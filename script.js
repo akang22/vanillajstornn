7c19fbc1533644890e033fdf1b521a36e74c10bc
Yanjun Kang <andrew22kang2004@gmail.com>
2021-10-11 20:40:31 -0700

    work on tensortools

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
-// Dear Future Reader Of My Code,
+/* Next 3 classes are tensors.
+ * Ideally, if this was java, I would have Add and Multiply inherit from Tensor. I don't know how to do prototypal OOP yet so I just won't have it here yet.
+ * Basically, these are classes that help you represent the way the weights and values become the final value. Doing it like this allows us to automate the differentiation / gradient calculation.
+ *
+ *
+ * TODO:
+ * - Add TDD for these classes
+ * - Continue on with using tensors for my Tensor operations.
+ */
+class TensorTools {
+	constructor() {
+		if (this instanceof TensorTools) {
+			throw Error('This class is intended to be static (used for its methods) and is not supposed to be instantiated.');
+		}
+	}
 
-// If this looks like Java, that's because it is.
-// I'm too lazy to learn how the normal javascript folks do it (well I do; using prototypes). 
-// But I think this is way better and safer for some reason.
-// I also was too lazy to learn how TypeScript differs from JavaScript.
-// Getting me one step closer to Java.
-// Too bad!
+	static assert(condition) {
+		if (!condition) {
+			throw new Error("Assertion failed");
+		}
+	}
 
-// Andrew Kang
+	static getNumDimensionsOf(ts) {
+		var dimCount = 0;
+		while (typeof ts != "number") {
+			ts = ts[0];
+			dimCount++;
+		}
+		return dimCount;
+	}
 
-function callFunction(func) {
-	return new function() { func() };
+	static getDimensionsOf(ts) {
+		var dimsSoFar = new Array(this.getNumDimensionsOf(ts));
+		var dimCount = 0;
+		while (typeof ts != "number") {
+			dimsSoFar[dimCount] = ts.length;
+			ts = ts[0];
+			dimCount++;
+		}
+		return dimsSoFar;
+	}
+
+	static flatten(ts) {
+		return [ts.flat(Infinity), this.getDimensionsOf(ts)];
+	}
+
+	static flattenedIndex(dims, access) {
+		var curIndex = 0;
+		this.assert(dims.length == access.length);
+		for (var i = 0; dims.length > i; i++) {
+			this.assert(dims[i] > access[i] && access[i] > 0);
+			curIndex *= dims[i];
+			curIndex += access[i];
+		}
+		return curIndex;
+	}
+
+	static zeroes(len) {
+		return new Array(len).fill(0);
+	}
 }
 
+
 class Tensor {
-	// item is the tensor (multidimensional array)
+	// arr is a multidimensional array)
 	// needsGrad is a boolean, asking if the gradient is used (and should be calculated when recursed on)
-	constructor(item, needsGrad) {
-		this.item = item;
+	constructor(arr, needsGrad) {
+		this.arr = arr;
 		this.needsGrad = needsGrad;
-		this.grad = 0;
+		this.grad_arr = TensorTools.zeros(arr.flat(Infinity).length);
 		this.name = "Tensor";
 	}
 
@@ -78,3 +126,4 @@ class Multiply {
 
 console.log("============LOGS START HERE============")
 
+arr = [[0, 1, 2, 3], [3, 2, 1, 0], [3, 5, 6, 1], [3, 8, 3, 4]]
